2249bd3d1a0af9f5d74f4878c65630c06273c97b
jaeyeon <kasto6329@gmail.com>
2026-05-14 11:54:29 +0900

    🐛 fix: Testcontainers PostGIS 이미지 사용

--- src/test/java/com/magampick/TestcontainersConfiguration.java
+++ src/test/java/com/magampick/TestcontainersConfiguration.java
@@ -12,6 +12,8 @@ class TestcontainersConfiguration {
   @Bean
   @ServiceConnection
   PostgreSQLContainer<?> postgresContainer() {
-    return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    return new PostgreSQLContainer<>(
+        DockerImageName.parse("postgis/postgis:16-3.4-alpine")
+            .asCompatibleSubstituteFor("postgres"));
   }
 }
